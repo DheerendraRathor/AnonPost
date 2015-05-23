ac0bfcedfab7784be75e226e4598281dca1e9c8c
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-05-24 02:43:26 +0530

    created complaint and Reply models

--- complaint/migrations/0001_initial.py
+++ complaint/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -15,6 +17,18 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('message', models.TextField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Reply',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('message', models.TextField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('complaint', models.ForeignKey(to='complaint.Complaint')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
--- complaint/models.py
+++ complaint/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Complaint(models.Model):
-	message = models.TextField()
+    user = models.ForeignKey(User)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Reply(models.Model):
+    user = models.ForeignKey(User)
+    complaint = models.ForeignKey(Complaint)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
