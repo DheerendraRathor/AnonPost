ae75d0293714bf2782e8e26af52e240f546e82c5
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-08-08 15:26:29 +0530

    moved secret key to settings_user

--- anon_post/settings.py
+++ anon_post/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^xl_*lzc=qbp!9vnrrzl(6nulb%ft3n(2qbrn4+ik9gv84d(2s'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
