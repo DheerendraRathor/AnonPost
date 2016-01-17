98a55eab379cff66a38740484740dd98149b60b3
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2016-01-17 13:55:55 +0000

    Hello World

--- anon_post/context.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-
-def admin_usernames(request):
-    return {'ADMIN_USERNAMES' : settings.ADMIN_USERNAMES}
--- anon_post/settings.py
+++ anon_post/settings.py
@@ -141,7 +141,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.core.context_processors.static',
-                'anon_post.context.admin_usernames',
             ],
         },
     },
